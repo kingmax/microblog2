04870f19b3f3ca151005a5c03b12e430f6a4b27f
kingmax <kingmax_res@163.com>
2020-11-05 00:53:09 +0800

    chapter2 test first page index.html

--- theApp/routes.py
+++ theApp/routes.py
@@ -7,4 +7,14 @@ from flask import render_template
 def index():
     # return 'Hello, World!'
     user = {'username': 'Jason'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in China!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
