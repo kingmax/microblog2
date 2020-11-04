152ff18efed2f6e2794a1d3f3624777750988a3f
kingmax <kingmax_res@163.com>
2020-11-05 01:28:52 +0800

    ch3 * Receiving Form Data

--- theApp/routes.py
+++ theApp/routes.py
@@ -1,6 +1,7 @@
 from theApp import app
 from flask import render_template
 from .forms import LoginForm
+from flask import flash, redirect
 
 
 @app.route('/')
@@ -21,7 +22,11 @@ def index():
     return render_template('index.html', title='Home', user=user, posts=posts)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Login requested for user {form.username.data}, '
+              f'remember_me={form.remember_me.data}')
+        return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
