19a0c10b2896fa421cbe8c299bcc95d3c0b5cceb
kingmax <kingmax_res@163.com>
2020-11-06 01:19:23 +0800

    + flask shell context

--- microblog.py
+++ microblog.py
@@ -1,1 +1,8 @@
 from theApp import app
+from theApp import db
+from theApp.models import User, Post
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
