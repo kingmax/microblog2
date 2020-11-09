e8fda774751ba01af34ad40ab4f0fedcea8c609e
kingmax <kingmax_res@163.com>
2020-11-10 00:48:37 +0800

    + user login

--- theApp/models.py
+++ theApp/models.py
@@ -2,9 +2,17 @@
 
 from theApp import db
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from theApp import login
 
 
-class User(db.Model):
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -15,6 +23,12 @@ class User(db.Model):
     def __repr__(self):
         return f'<User {self.username}>'
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -24,3 +38,5 @@ class Post(db.Model):
 
     def __repr__(self):
         return f'<Post {self.body}>'
+
+
